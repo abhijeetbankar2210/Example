dea84c4dd0189c0c8ebc3c174917e278b6716ea5
abhijeet bankar <abhijeetbankar178@gmail.com>
2026-05-02 15:18:55 +0530

    this is just for the example

---
+++ eclipse-workspaceapri17/example/src/example/Result.java
@@ -0,0 +1,34 @@
+package example;
+
+
+
+
+public class Result {
+	public static void main(String[] args) {
+		int a=84;
+		int b=92;
+		int c=93;
+		
+		if(a>b&&a>c) 
+		{     
+			
+			System.out.println("a is the biggest number");
+		}
+		if(b>a&&b>c) {
+			 System.out.println("b is the biggest number");		
+		}
+		else{
+			System.out.println("c is the greatest number ");
+			
+					
+				
+				}
+					
+			}
+		
+		
+		
+		
+	}
+
+
